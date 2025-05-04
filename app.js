63773920d1655b08ea060f067b31bad66f31e157
preetikumari15 <annukumari0928@gmail.com>
2025-05-05 00:11:32 +0530

    register

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userRouter = require('./routes/user.routes')
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/user', userRouter)
 
