cce02e5789267cfb080597abbefaeafd01f28b79
preetikumari15 <annukumari0928@gmail.com>
2025-05-04 23:32:20 +0530

    ejs setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userRouter = require('./routes/user.routes')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/user', userRouter)
+
+app.listen(3000,() =>{
+    console.log("Server is running on port 3000");
+    
+})
